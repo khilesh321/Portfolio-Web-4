f7b86472efc9745ef4a503c0327d51b25cbf9685
Khilesh Jawale <khilujawale321@gmail.com>
2024-11-02 00:09:35 +0530

    add mouse follower anims and more anims

--- script.js
+++ script.js
@@ -61,3 +61,35 @@ function restrict(){
   }
 
 restrict();
+
+// shery
+
+Shery.imageEffect(".highlight-projects", {
+  style: 5,
+  config: {"a":{"value":1.49,"range": [0,30]}, "b":{"value":-0.98, "range" : [-1,1]}, "aspect": {"value":1.8822947576656774},"gooey": {"value":true}, "infiniteGooey": {"value":true}, "durationOut": {"value":1,"range" : [0.1,5]},"durationIn": {"value":1,"range": [0.1,5]}, "displaceAmount":{"value":0.5}, "masker": {"value":true}, "maskVal": {"value":1.33, "range" : [1,5]},"scrollType":{"value":0},"geoVertex":{"range": [1,64], "value":1}, "noEffectGooey": {"value":false}, "onMouse": {"value":0}, "noise_speed":{"value":1.59, "range" : [0,10]}, "metaball":{"value":0.21, "range" : [0,2]}, "discard_threshold":{"value":0.5,"range" : [0,1]}, "antialias_threshold":{"value": 0, "range" : [0,0.1]}, "noise_height":{"value":0.47,"range" : [0,2]}, "noise_scale":{"value":12.15, "range" : [0,100]}},
+  gooey: true,
+
+});
+
+Shery.mouseFollower({
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+// for removing the mouse follower
+Shery.imageMasker(".highlight-projects img" /* Element to target.*/, {
+  mouseFollower: false,
+  text: "",
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.textAnimate(".text-target", {
+  style: 2,
+  y: 10,
+  delay: 0,
+  duration: 0.5,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0,
+});
