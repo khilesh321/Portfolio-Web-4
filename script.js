53dcfa34f52742f43ec1dfef60e95c9098cfad4a
Khilesh Jawale <khilujawale321@gmail.com>
2024-11-02 16:46:56 +0530

    added proect name and fix image not loading issue & fix the nav bar redirect

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const lenis = new Lenis({
   // other configurations
 });
 
-
 // Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
   lenis.raf(time);
@@ -14,11 +13,11 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 // restriction
-function restrict(){
+function restrict() {
   // for disable right click
-  document.addEventListener('contextmenu', event => event.preventDefault());
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-  document.onkeydown = function(e) {
+  document.onkeydown = function (e) {
     // Disable F12 key
     if (e.keyCode == 123) {
       return false;
@@ -37,68 +36,32 @@ function restrict(){
     }
   };
 
-
   // Disable dragging for images and videos
-  document.addEventListener('dragstart', function(e) {
-    if (e.target.nodeName === 'IMG' || e.target.nodeName === 'VIDEO') {
+  document.addEventListener("dragstart", function (e) {
+    if (e.target.nodeName === "IMG" || e.target.nodeName === "VIDEO") {
       e.preventDefault();
     }
   });
 
-
   // Disable text selection
-  document.addEventListener('selectstart', function(e) {
+  document.addEventListener("selectstart", function (e) {
     e.preventDefault();
   });
 
   // Disable copy event
-  document.addEventListener('copy', function(e) {
+  document.addEventListener("copy", function (e) {
     e.preventDefault();
   });
 
   // Disable cut event
-  document.addEventListener('cut', function(e) {
+  document.addEventListener("cut", function (e) {
     e.preventDefault();
   });
-
-  }
+}
 
 // restrict();
 
 // shery
-
-Shery.imageEffect(".listProject .item img", {
-  style: 1,
-  config: {"a":{"value":1.49,"range": [0,30]}, "b":{"value":-0.98, "range" : [-1,1]}, "aspect": {"value":1.8822947576656774},"gooey": {"value":true}, "infiniteGooey": {"value":true}, "durationOut": {"value":1,"range" : [0.1,5]},"durationIn": {"value":1,"range": [0.1,5]}, "displaceAmount":{"value":0.5}, "masker": {"value":true}, "maskVal": {"value":1.33, "range" : [1,5]},"scrollType":{"value":0},"geoVertex":{"range": [1,64], "value":1}, "noEffectGooey": {"value":false}, "onMouse": {"value":0}, "noise_speed":{"value":1.59, "range" : [0,10]}, "metaball":{"value":0.21, "range" : [0,2]}, "discard_threshold":{"value":0.5,"range" : [0,1]}, "antialias_threshold":{"value": 0, "range" : [0,0.1]}, "noise_height":{"value":0.47,"range" : [0,2]}, "noise_scale":{"value":12.15, "range" : [0,100]}},
-});
-
-Shery.imageEffect(".page4 .container div", {
-  style: 5,
-  config: {
-    "a": {"value": 1.49, "range": [0, 30]},
-    "b": {"value": -0.98, "range": [-1, 1]},
-    "aspect": {"value": 1.8822947576656774},
-    "gooey": {"value": true},
-    "infiniteGooey": {"value": true},
-    "durationOut": {"value": 1, "range": [0.1, 5]},
-    "durationIn": {"value": 1, "range": [0.1, 5]},
-    "displaceAmount": {"value": 1.0}, // Increased displacement
-    "masker": {"value": true},
-    "maskVal": {"value": 2.0, "range": [1, 5]}, // Increased mask value
-    "scrollType": {"value": 0},
-    "geoVertex": {"range": [1, 64], "value": 1},
-    "noEffectGooey": {"value": false},
-    "onMouse": {"value": 0},
-    "noise_speed": {"value": 1.59, "range": [0, 10]},
-    "metaball": {"value": 0.5, "range": [0, 2]}, // Increased metaball intensity
-    "discard_threshold": {"value": 0.5, "range": [0, 1]}, // Lowered discard threshold
-    "antialias_threshold": {"value": 0, "range": [0, 0.1]},
-    "noise_height": {"value": 0.47, "range": [0, 2]},
-    "noise_scale": {"value": 12.15, "range": [0, 100]}
-  },
-  gooey: true,
-});
-
 
 // Function to detect if the user is on a mobile device
 function isMobileDevice() {
@@ -112,97 +75,142 @@ if (!isMobileDevice()) {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
+  document.addEventListener("DOMContentLoaded", function () {
+    Shery.imageEffect(".listProject .item img", {
+      style: 3,
+      config: {
+        a: { value: 1.05, range: [0, 30] }, // Reduced 'a' for minimal zoom
+        b: { value: -0.3, range: [-1, 1] }, // Further adjusted 'b' for subtlety
+        aspect: { value: 1.3 }, // Adjusted for a softer effect
+        gooey: { value: true },
+        infiniteGooey: { value: true },
+        durationOut: { value: 0.6, range: [0.1, 5] }, // Faster transition out
+        durationIn: { value: 0.6, range: [0.1, 5] }, // Faster transition in
+        displaceAmount: { value: 0.2 }, // Significantly reduced displacement
+        masker: { value: true },
+        maskVal: { value: 1.2, range: [1, 5] }, // Reduced mask value for subtler effect
+        scrollType: { value: 0 },
+        geoVertex: { range: [1, 64], value: 1 },
+        noEffectGooey: { value: false },
+        onMouse: { value: 0 },
+        noise_speed: { value: 0.5, range: [0, 10] }, // Slower noise speed
+        metaball: { value: 0.1, range: [0, 2] }, // Very low metaball intensity
+        discard_threshold: { value: 0.8, range: [0, 1] }, // Higher discard threshold
+        antialias_threshold: { value: 0, range: [0, 0.1] },
+        noise_height: { value: 0.1, range: [0, 2] }, // Lower noise height
+        noise_scale: { value: 5, range: [0, 100] }, // Reduced noise scale
+      },
+    });
+  });
+
+  Shery.imageEffect(".page4 .container div", {
+    style: 5,
+    config: {
+      a: { value: 1.49, range: [0, 30] },
+      b: { value: -0.98, range: [-1, 1] },
+      aspect: { value: 1.8822947576656774 },
+      gooey: { value: true },
+      infiniteGooey: { value: true },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1, range: [0.1, 5] },
+      displaceAmount: { value: 1.0 }, // Increased displacement
+      masker: { value: true },
+      maskVal: { value: 2.0, range: [1, 5] }, // Increased mask value
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: false },
+      onMouse: { value: 0 },
+      noise_speed: { value: 1.59, range: [0, 10] },
+      metaball: { value: 0.5, range: [0, 2] }, // Increased metaball intensity
+      discard_threshold: { value: 0.5, range: [0, 1] }, // Lowered discard threshold
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.47, range: [0, 2] },
+      noise_scale: { value: 12.15, range: [0, 100] },
+    },
+    gooey: true,
+  });
+  // for removing the mouse follower
+  Shery.imageMasker(".find ul li" /* Element to target.*/, {
+    mouseFollower: false,
+    text: "",
+    ease: "ease-in-out",
+    duration: 0.1,
+  });
+  Shery.imageMasker(".page4 .container div" /* Element to target.*/, {
+    mouseFollower: false,
+    text: "",
+    ease: "ease-in-out",
+    duration: 0.1,
+  });
+
+  Shery.textAnimate(".text-target", {
+    style: 2,
+    y: 10,
+    delay: 0,
+    duration: 0.5,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0,
+  });
 }
 
-
-// for removing the mouse follower
-Shery.imageMasker(".find ul li" /* Element to target.*/, {
-  mouseFollower: false,
-  text: "",
-  ease: "ease-in-out",
-  duration: 0.1,
-});
-Shery.imageMasker(".page4 .container div" /* Element to target.*/, {
-  mouseFollower: false,
-  text: "",
-  ease: "ease-in-out",
-  duration: 0.1,
-});
-
-// for removing the mouse follower
-Shery.imageMasker(".highlight-projects img" /* Element to target.*/, {
-  mouseFollower: false,
-  text: "",
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
-
-Shery.textAnimate(".text-target", {
-  style: 2,
-  y: 10,
-  delay: 0,
-  duration: 0.5,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  multiplier: 0,
-});
-
-
-// cursor effect 
-var TxtType = function(el, toRotate, period) {
+// cursor effect
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
   var that = this;
   var delta = 200 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+  if (this.isDeleting) {
+    delta /= 2;
   }
 
-  setTimeout(function() {
-  that.tick();
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
   }, delta);
 };
 call1();
 function call1() {
   document.querySelector(".blink-border").style.visibility = "visible";
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".typewrite > .wrap { border-right: 0em solid gray}";
   document.body.appendChild(css);
-};
+}
 // // //
