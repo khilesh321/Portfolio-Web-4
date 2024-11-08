6fe7ec2cfdb6c364f1661dcbee89231d3e0cd67f
Khilesh Jawale <khilujawale321@gmail.com>
2024-11-08 10:08:27 +0530

    make the animation more smooth of .page4 and set display none of .video-cont for less than 767px devices

--- script.js
+++ script.js
@@ -216,32 +216,34 @@ if (!isMobileDevice()) {
       applySheryEffect();
     }
 
-    Shery.imageEffect(".page4 .container div", {
-      style: 4,
-      config: {
-        a: { value: 1.49, range: [0, 30] },
-        b: { value: -0.98, range: [-1, 1] },
-        aspect: { value: 1.8822947576656774 },
-        gooey: { value: true },
-        infiniteGooey: { value: true },
-        durationOut: { value: 1, range: [0.1, 5] },
-        durationIn: { value: 1, range: [0.1, 5] },
-        displaceAmount: { value: 1.0 }, // Increased displacement
-        masker: { value: true },
-        maskVal: { value: 2.0, range: [1, 5] }, // Increased mask value
-        scrollType: { value: 0 },
-        geoVertex: { range: [1, 64], value: 1 },
-        noEffectGooey: { value: false },
-        onMouse: { value: 0 },
-        noise_speed: { value: 1.59, range: [0, 10] },
-        metaball: { value: 0.5, range: [0, 2] }, // Increased metaball intensity
-        discard_threshold: { value: 0.5, range: [0, 1] }, // Lowered discard threshold
-        antialias_threshold: { value: 0, range: [0, 0.1] },
-        noise_height: { value: 0.47, range: [0, 2] },
-        noise_scale: { value: 12.15, range: [0, 100] },
-      },
-      gooey: true,
-    });
+    for(let i=1; i<=3; i++){
+      Shery.imageEffect(`.page4 .container .pic${i}`, {
+        style: 4,
+        config: {
+          a: { value: 1.49, range: [0, 30] },
+          b: { value: -0.98, range: [-1, 1] },
+          aspect: { value: 1.8822947576656774 },
+          gooey: { value: true },
+          infiniteGooey: { value: true },
+          durationOut: { value: 1, range: [0.1, 5] },
+          durationIn: { value: 1, range: [0.1, 5] },
+          displaceAmount: { value: 1.0 }, // Increased displacement
+          masker: { value: true },
+          maskVal: { value: 2.0, range: [1, 5] }, // Increased mask value
+          scrollType: { value: 0 },
+          geoVertex: { range: [1, 64], value: 1 },
+          noEffectGooey: { value: false },
+          onMouse: { value: 0 },
+          noise_speed: { value: 1.59, range: [0, 10] },
+          metaball: { value: 0.5, range: [0, 2] }, // Increased metaball intensity
+          discard_threshold: { value: 0.5, range: [0, 1] }, // Lowered discard threshold
+          antialias_threshold: { value: 0, range: [0, 0.1] },
+          noise_height: { value: 0.47, range: [0, 2] },
+          noise_scale: { value: 12.15, range: [0, 100] },
+        },
+        gooey: true,
+      });
+    }
 
     // for removing the mouse follower
     Shery.imageMasker(".find ul li" /* Element to target.*/, {
